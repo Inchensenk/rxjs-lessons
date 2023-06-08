874d76ff3a60f402feb69aeaa49fcaef0425a6e1
DreameR <aab8117@gmail.com>
2023-06-08 11:58:12 +0300

    ...

--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -4,17 +4,28 @@ import {Observable} from "rxjs";
 const search$ = new Observable(observer => {
   const search = document.querySelector('#search');
 
+  if (!search){
+    observer.error('Element does not exist on the page');
+    return;
+  }
+
   search!.addEventListener('input', event => {
     observer.next(event);
+    observer.complete();
   });
 });
 
 
-search$.subscribe(value =>{
-  console.log(value);
+search$.subscribe({
+
+  next: value => {
+    console.log(value);
+  },
+
+  complete: () => {
+    console.log('event end');
+  }
+
 });
 
-search$.subscribe(value =>{
-  console.log(value);
-});
 
